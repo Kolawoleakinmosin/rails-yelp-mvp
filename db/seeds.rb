7342127149864ba08be20342155f1cfdfa752564
kolawole akinmosin <kolawoleakinmosin01@gmail.com>
2023-03-08 11:48:23 +0000

    restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Deleting restaurants'
+Restaurant.destroy_all
+
+puts '********************'
+puts 'Creating new restaurants'
+Restaurant.create!(name: 'road to lagos', address: 'lagos', phone_number: '09151708213', category: 'chinese')
+Restaurant.create!(name: 'shawarma lounge', address: 'abuja', phone_number: '09151674843', category: 'italian')
+Restaurant.create!(name: 'everybody chop', address: 'ogun', phone_number: '09151708213', category: 'japanese')
+Restaurant.create!(name: 'circa', address: 'ondo', phone_number: '09151708213', category: 'belgian')
+Restaurant.create!(name: 'blackbell', address: 'kano', phone_number: '09151708213', category: 'french')
+
+puts '*********************'
+
+puts 'finished'
